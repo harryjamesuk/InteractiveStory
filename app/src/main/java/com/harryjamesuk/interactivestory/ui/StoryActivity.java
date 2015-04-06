c5f619727999265b017185c79f8721dbea3dcef1
Harry James <harryjamesuk@users.noreply.github.com>
2015-04-06 19:40:18 +0100

    Now loading and using the page
    Updated member variable formatting.
    Created new member variable String mName.
    mName will be set to the name passed in by the Intent.
    
    Refactored name to mName.
    
    Now running loadPage() from onCreate().
    
    Rather than just displaying the text straight away, we'll now format the
    String so we replace the %1$s in the Story text with the parameter,
    mName.
    The %1 stands for parameter 1 and $s stands for String.
    Only after formatting the text will we set it now.

--- app/src/main/java/com/harryjamesuk/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/harryjamesuk/interactivestory/ui/StoryActivity.java
@@ -19,10 +19,13 @@ public class StoryActivity extends Activity {
     public static final String TAG = StoryActivity.class.getSimpleName();
 
     private Story mStory = new Story();
+
     private ImageView mImageView;
     private TextView mTextView;
     private Button mChoice1;
     private Button mChoice2;
+
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,17 +33,19 @@ public class StoryActivity extends Activity {
         setContentView(R.layout.activity_story);
 
         Intent intent = getIntent();
-        String name = intent.getStringExtra(getString(R.string.key_name));
+        mName = intent.getStringExtra(getString(R.string.key_name));
 
-        if (name == null) {
-            name = "Friend";
+        if (mName == null) {
+            mName = "Friend";
         }
-        Log.d(TAG, name);
+        Log.d(TAG, mName);
 
         mImageView = (ImageView) findViewById(R.id.storyImageView);
         mTextView = (TextView) findViewById(R.id.storyTextView);
         mChoice1 = (Button) findViewById(R.id.choiceButton1);
         mChoice2 = (Button) findViewById(R.id.choiceButton2);
+
+        loadPage();
     }
 
     private void loadPage() {
@@ -49,7 +54,10 @@ public class StoryActivity extends Activity {
         Drawable drawable = getResources().getDrawable(page.getImageId());
         mImageView.setImageDrawable(drawable);
 
-        mTextView.setText(page.getText());
+        String pageText = page.getText();
+        // Add the name if placeholder included. Won't add if no placeholder :)
+        pageText = String.format(pageText, mName);
+        mTextView.setText(pageText);
 
         mChoice1.setText(page.getChoice1().getText());
         mChoice2.setText(page.getChoice2().getText());

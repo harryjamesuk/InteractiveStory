06c5276bc511f18c09f048fbdc3d2263c450bf0c
Harry James <harryjamesuk@users.noreply.github.com>
2015-04-06 19:07:38 +0100

    Set up page getting/loading
    Created getPage() method in Story.java that takes in the pageNumber,
    returning the Page object.
    
    Updated StoryActivities onCreate() method to be initialized with all of
    the page elements.
    New loadPage() method sets all of the page elements for the page
    (Currently only the page at index 0).

--- app/src/main/java/com/harryjamesuk/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/harryjamesuk/interactivestory/ui/StoryActivity.java
@@ -2,15 +2,27 @@ package com.harryjamesuk.interactivestory.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.harryjamesuk.interactivestory.R;
+import com.harryjamesuk.interactivestory.model.Page;
+import com.harryjamesuk.interactivestory.model.Story;
 
 
 public class StoryActivity extends Activity {
 
     public static final String TAG = StoryActivity.class.getSimpleName();
+
+    private Story mStory = new Story();
+    private ImageView mImageView;
+    private TextView mTextView;
+    private Button mChoice1;
+    private Button mChoice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +36,22 @@ public class StoryActivity extends Activity {
             name = "Friend";
         }
         Log.d(TAG, name);
+
+        mImageView = (ImageView) findViewById(R.id.storyImageView);
+        mTextView = (TextView) findViewById(R.id.storyTextView);
+        mChoice1 = (Button) findViewById(R.id.choiceButton1);
+        mChoice2 = (Button) findViewById(R.id.choiceButton2);
+    }
+
+    private void loadPage() {
+        Page page = mStory.getPage(0);
+
+        Drawable drawable = getResources().getDrawable(page.getImageId());
+        mImageView.setImageDrawable(drawable);
+
+        mTextView.setText(page.getText());
+
+        mChoice1.setText(page.getChoice1().getText());
+        mChoice2.setText(page.getChoice2().getText());
     }
 }
